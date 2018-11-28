71ec15d5bab2f78c7faab986a0c5ef4c21b6d9d8
Dawei Sun <dsun2016@gmail.com>
2018-11-28 10:01:19 -0800

    made a change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 class Header extends Component {
     render() {
+      console.log("asdfasdf");
         return (
             <div >
                 <header className="header">My Orders</header>
